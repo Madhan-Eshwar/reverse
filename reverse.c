8c5eea947080af9b411926b98d61eff2e8020bdb
Madhan-Eshwar <mailtomadhan95@gmail.com>
2016-08-13 13:38:55 +0530

    Update reverse.c

--- reverse.c
+++ reverse.c
@@ -1,1 +1,17 @@
-
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+int main()
+{
+  char a[100];
+  scanf("%s",a);
+  int n;
+  n=strlen(a);
+  
+  for(i=n;i>=0;i--)
+  {
+    printf("%c",a[i]);
+  }
+  
+  return 0;
+}
